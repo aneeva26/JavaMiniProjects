f99c8fb9f4005e300f62b55485ad6e584acf4759
Aneeva Murray <74313463+aneeva26@users.noreply.github.com>
2020-11-16 18:21:44 -0500

    Add files via upload

---
+++ VariablesAndDataTypes.java
@@ -0,0 +1,30 @@
+package variablesanddatatypes;
+
+//strings are letters or strings of characters from your keyboard
+//integers are whole numbers that you can do calculations with
+
+public class VariablesAndDataTypes {
+
+    public static void main(String[] args) {
+        int StudentID = 739907;
+        String Grade1 = "86";   
+        String Grade2 = "90";
+        String Grade3 = "70";
+        String Grade4 = "82";
+        String Grade5 = "80";
+        
+        int FinalGrade;
+        Integer.parseInt(Grade1);
+        Integer.parseInt(Grade2);
+        Integer.parseInt(Grade3);
+        Integer.parseInt(Grade4);
+        Integer.parseInt(Grade5);
+        
+        System.out.println(StudentID);
+        System.out.println(86 + 90 + 70 + 82 + 80);
+        System.out.println("Your average is " + 408 / 5);     
+    
+    
+    }
+    
+}
